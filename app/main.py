98463f2fc39c8fe6b79156356f81e34e051c955c
Rui Dai <dairuifromcd@gmail.com>
2025-01-31 20:11:25 +1000

    fix lint issue

--- app/main.py
+++ app/main.py
@@ -5,4 +5,8 @@ from app.api.v1.measurement import router as measurement_router
 app = FastAPI(title="Track Kids Measurements API", version="0.1")
 
 app.include_router(child_router, prefix="/api/v1/children", tags=["children"])
-app.include_router(measurement_router, prefix="/api/v1/measurements", tags=["measurements"])
+app.include_router(
+    measurement_router,
+    prefix="/api/v1/measurements",
+    tags=["measurements"]
+)
